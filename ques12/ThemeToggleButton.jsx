7702a54d08de6f2f7f1d393803e59494e8e70cf1
Istuti Chhabra <102655463+istutichhabra1@users.noreply.github.com>
2025-05-01 20:56:22 +0530

    Create ThemeToggleButton.jsx

---
+++ ques12/ThemeToggleButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useTheme } from './ThemeContext';
+
+const ThemeToggleButton = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <button onClick={toggleTheme}>
+      Toggle to {theme === 'light' ? 'Dark' : 'Light'} Theme
+    </button>
+  );
+};
+
+export default ThemeToggleButton;
